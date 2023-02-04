e479b6da2470e033c43a08fae85269a128995bf8
Craig Ostrin <40326682+craigostrin@users.noreply.github.com>
2023-02-03 16:15:42 -0800

    Finish basic contact input page I might need to add <form> tags here. Learning about that now.

--- address-book.js
+++ address-book.js
@@ -1,3 +1,9 @@
 $("button#add-contact").click( () => {
     window.location = "contacts/input-contact.html"
 });
+
+/* JavaScript runs on client side, not server side. So it can't create new files.
+    So, instead, we will store contact info in dictionaries, and when someone clicks the 
+    link corresponding with that contact-dictionary, the contact page will populate with
+    data from the relevant dictionary.
+*/
